f9d948d142a0a8e0dd5b4df79b9aa1511529dc54
afzheng <afzheng@live.com>
2017-08-03 00:00:19 +0800

    增加前端页面主题格局

---
+++ src/page/Info.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="info">
+    i am info
+  </div>
+</template>
+
+<script>
+  export default {
+    name:"info"
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
